6eb5190f7535c9503f1ec3885516d78a43124f60
Richard Rodriguez <rmjuarez12@gmail.com>
2025-11-18 23:33:32 -0500

    removed unecessary print

--- scenes/GameManagementScenes/GameManager/scripts/game_manager.gd
+++ scenes/GameManagementScenes/GameManager/scripts/game_manager.gd
@@ -2,6 +2,3 @@ extends Node
 
 @onready var player_one_node: CharacterBody2D = get_node("../Player1")
 @onready var player_two_node: CharacterBody2D = get_node("../Player2")
-
-func _ready() -> void:
-  print(player_one_node)
